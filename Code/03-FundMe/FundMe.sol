bf9751bfa056ab33d1c74b45625a1ed0b63048a6
gandalf-blockchain <gandalfblockchain@gmail.com>
2025-12-10 03:03:48 +0700

    Update FundMe.sol

--- Code/03-FundMe/FundMe.sol
+++ Code/03-FundMe/FundMe.sol
@@ -1,1 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
 
+//import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 public minimum_Usd = 5e18;
+    address[] public funders; 
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+
+    address public owner; 
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function fund() public payable {
+        require(msg.value.getConversionRate() >= minimum_Usd, "You need to spend more ETH!");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function withdraw() public onlyOwner {
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+        address funder = funders[funderIndex];
+        addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        // // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+
+        // call
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Send failed");
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Sender Is Not Owner");
+        _;
+    }
+
+    fallback() external payable {
+        fund();
+    }
+
+    receive() external payable {
+        fund();
+    }
+}
